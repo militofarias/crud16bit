4b34c65e1e7714b373a2f30a78567e1ae5105721
militofarias <MilitoFarias@MacMilito.local>
2017-04-26 02:27:32 -0300

    html, CSS e spec.ts consertados

--- src/app/spec.js
+++ src/app/spec.js
@@ -1,15 +1,15 @@
 // spec.js
-describe('CRUD 16-bit app', function() {
+/*describe('CRUD 16-bit app', function() {
   it('Deve ter um titulo', function() {
     browser.get('http://localhost:4200/');
 
     expect(browser.getTitle()).toEqual('Form16bitApp');
   });
-});
+});*/
 
 describe('Teste de Insercão de dados', function(){
     it('Deve adicionar dados a linha da tabela de estampas', function(){
-        browser.get('http://diegocoelho.me:4200/');
+        browser.get('http://localhost:4200/');
         element(by.id('form-nome')).sendKeys('Teste1');
         element(by.id('form-tamP')).sendKeys(1);
         element(by.id('form-tamM')).sendKeys(2);
@@ -18,11 +18,11 @@ describe('Teste de Insercão de dados', function(){
 
         element(by.id('btnSend')).click();
 
-        expect(element(by.binding('item.nome')).getText()).toEqual('Teste1');
-        expect(element(by.binding('item.tamP')).getText()).toEqual('1');
-        expect(element(by.binding('item.tamM')).getText()).toEqual('2');
-        expect(element(by.binding('item.tamG')).getText()).toEqual('3');
-        expect(element(by.binding('item.tamGG')).getText()).toEqual('4');
+        expect(element(by.id('tabNome')).getText()).toEqual('Teste1');
+        expect(element(by.id('tabTamP')).getText()).toEqual('1');
+        expect(element(by.id('tabTamM')).getText()).toEqual('2');
+        expect(element(by.id('tabTamG')).getText()).toEqual('3');
+        expect(element(by.id('tabTamGG')).getText()).toEqual('4');
 
     });
 });
